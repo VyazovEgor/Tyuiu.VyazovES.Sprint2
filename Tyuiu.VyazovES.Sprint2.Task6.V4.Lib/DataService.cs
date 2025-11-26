58646b51d0f18b7dfcafd58832a965ecf1b1fc62
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-26 22:46:50 +0500

    123

---
+++ Tyuiu.VyazovES.Sprint2.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VyazovES.Sprint2.Task6.V4.Lib
+{
+    public class DataService : ISprint2Task6V4
+    {
+        public string FindCardSuit(int x)
+        {
+
+            string suitName = x switch
+            {
+                1 => "пики",
+                2 => "трефы",
+                3 => "бубны",
+                4 => "червы",
+                _ => "неизвестная масть. Введите число от 1 до 4."
+            };
+
+                return suitName;
+            
+        }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint2.Task6.V4/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.VyazovES.Sprint2.Task6.V4.Lib;
+
+DataService ds = new DataService();
+
+int x = Convert.ToInt32(Console.ReadLine());
+string z = ds.FindCardSuit(x);
+Console.WriteLine(z);
+Console.ReadKey();
