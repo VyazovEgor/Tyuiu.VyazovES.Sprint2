7a9e01862a7336e6fa87f263a79e98675220b6ae
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-26 22:05:32 +0500

    4

---
+++ Tyuiu.VyazovES.Sprint2.Task4.V25.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VyazovES.Sprint2.Task4.V25.Lib
+{
+    public class DataService : ISprint2Task4V25
+    {
+        public double Calculate(double x, double y)
+        {
+            double z;
+
+            // Используем тернарный оператор для вычисления z
+            z = (x - 20 * 2 < y + 4)
+                ? Math.Pow(1 + 2 / Math.Pow(x, 2), y)
+                : y + Math.Pow((x + 1) / (y + 2), x); // Предполагаем, что z в правой части - это x
+
+            // Округление до трех знаков после запятой
+            return z = Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint2.Task4.V25/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.VyazovES.Sprint2.Task4.V25.Lib;
+
+DataService ds = new DataService();
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+double z= ds.Calculate(x, y);
+Console.WriteLine(y);
+Console.ReadKey();
