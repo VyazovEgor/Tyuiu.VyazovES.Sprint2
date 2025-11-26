1270472509d5015cbbbe5272c81b43d60cf13da8
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-26 21:31:52 +0500

    push

---
+++ Tyuiu.VyazovES.Sprint2.Task2.V20.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.VyazovES.Sprint2.Task2.V20.Lib
+{
+    public class DataService : ISprint2Task2V20
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res =
+            (x == 3 && (y == 3 || y == 4 || y == 5 || y == 9)) ||
+            (x == 4 && (y == 3 || y == 4 || y == 5 || y == 9)) ||
+            (x == 5 && y >= 3 && y <= 9) ||
+            (x == 6 && (y == 3 || (y >= 6 && y <= 9) || y == 13)) ||
+            (x == 7 && ((y >= 6 && y <= 9) || y == 13)) ||
+            (x == 8 && (y >= 6 && y <= 13)) ||
+            (x == 9 && (y == 6 || (y >= 9 && y <= 12))) ||
+            (x == 10 && (y == 6 || (y >= 9 && y <= 12))) ||
+            (x == 11 && ((y >= 3 && y <= 6) || (y >= 9 && y <= 12))) ||
+            (x == 12 && (y >= 9 && y <= 12)) ||
+            (x == 13 && (y >= 10 && y <= 12));
+
+            if (res)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint2.Task2.V20/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.VyazovES.Sprint2.Task2.V20.Lib;
+
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+DataService ds = new DataService();
+bool res;
+res = ds.CheckDotInShadedArea(x, y);
+
+if (res)
+    Console.WriteLine("Точка находится в заштрихованной области.");
+else
+    Console.WriteLine("Точка не находится в заштрихованной области.");
+
+Console.ReadKey();
