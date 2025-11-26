533ec3595f279b59d0cdb0b87e86e5ffd1981b56
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-11-26 19:02:58 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.VyazovES.Sprint2.Task0.V3.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.VyazovES.Sprint2.Task0.V3.Lib
+{
+    public class DataService : ISprint2Task0V3
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = (x + 0) == (y + 0);
+            res[1] = (x + 0) != (y + 0);
+            res[2] = (x + 100) < (y + 0);
+            res[3] = (x + 100) > (y + 0);
+            res[4] = (x + 0) <= (y - 100);
+            res[5] = (x + 0) >= (y - 100);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint2.Task0.V3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.VyazovES.Sprint2.Task0.V3.Lib;
+
+DataService ds = new DataService();
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(45, 127);
+
+for (int i = 0; i < 6; i++) { 
+Console.WriteLine(res[i]);
+}
+
+Console.ReadKey();
